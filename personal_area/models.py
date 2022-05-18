c953268b3b7f00b4630d3ededacb9cd9662bd845
Den4ik-Bro <denis.rr@bk.ru>
2022-05-18 22:52:39 +0300

    add model, edit settings.py and admin.py. Add templates

--- personal_area/models.py
+++ personal_area/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Skills(models.Model):
+    skill = models.CharField(max_length=50, verbose_name='навык')
+    user = models.ManyToManyField(User, related_name='skills', verbose_name='навыки', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
+
+    def __str__(self):
+        return self.skill
